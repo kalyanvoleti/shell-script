8c822770f8d0bfd39e8ecb0429f171783c8b24c1
Kalyan Kumar Voleti <kalyankumarvoleti@gmail.com>
2025-08-08 23:42:47 +0530

    feat(variables): add script demonstrating variable usage with a friendly conversation

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# No spaces around '=' in variable assignment
+
+PERSON1=Calvin
+PERSON2=Levi
+
+echo "$PERSON1: Hey $PERSON2, excited for the weekend plans?"
+echo "$PERSON2: Definitely! Can’t wait to hit the trails."
+echo "$PERSON1: Same here, it’s going to be fun and refreshing."
+echo "$PERSON2: Absolutely, looking forward to it!"

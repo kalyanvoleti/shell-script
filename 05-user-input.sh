7dca336740d83e868eeed790974eeb7abdc4b767
Kalyan Kumar Voleti <kalyankumarvoleti@gmail.com>
2025-08-17 23:27:15 +0530

    feat(input): prompt for username and password securely

---
+++ 05-user-input.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your username::" # Text entered here will be stored in the variable
+
+read -s USERNAME # USERNAME is defined as the variable name
+
+echo "username entered: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD
